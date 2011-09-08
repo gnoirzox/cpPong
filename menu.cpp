961aa2ad5cbaeb93226e5a88c13af5e2cca87cdf
gnoirzox <gnoirzox@gmail.com>
2011-09-08 22:55:47 +0200

    add menu files

---
+++ menu.cpp
@@ -0,0 +1,11 @@
+/*
+ *  menu.cpp
+ *  cpPong
+ *
+ *  Created by Simon Rouger on 06/09/11.
+ *  Copyright 2011 supinfo. All rights reserved.
+ *
+ */
+
+#include "menu.h"
+
---
+++ menu.h
@@ -0,0 +1,9 @@
+/*
+ *  menu.h
+ *  cpPong
+ *
+ *  Created by Simon Rouger on 06/09/11.
+ *  Copyright 2011 supinfo. All rights reserved.
+ *
+ */
+
